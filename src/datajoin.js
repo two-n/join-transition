d8cc253718bab99dffe3d37a56ae0458475558bc
Paul Dechov <dechovsky@gmail.com>
2016-07-13 13:42:28 -0400

    Refactor underscore dependency out of datajoin

--- src/datajoin.js
+++ src/datajoin.js
@@ -1,25 +1,33 @@
-import { defaults, groupBy, reject, partition } from "underscore"
+
+const groupBy = (collection, accessor) => {
+  const byValue = {}
+  for (let i = 0; i < collection.length; i++) {
+    const value = accessor ? accessor(collection[i]) : collection[i]
+    byValue[value] = (byValue[value] || []).concat(collection[i])
+  }
+  return byValue
+}
 
 export default (A=[], B=[], options={}) => {
-  options = defaults(options, {
+  options = Object.assign({
     key: "id",
     exitTo: Object,
     enterFrom: Object,
     keyTo: null,
     keyFrom: null,
-  })
+  }, options)
 
-  const keyTo = options.keyTo != null ? options.keyTo : options.key
-  const keyFrom = options.keyFrom != null ? options.keyFrom : options.key
+  const keyTo = options.keyTo != null ? options.keyTo : options.key,
+        keyFrom = options.keyFrom != null ? options.keyFrom : options.key
 
-  const groupA = groupBy(A, keyTo)
-  const groupB = groupBy(B, keyFrom)
+  const groupA = groupBy(A, keyTo),
+        groupB = groupBy(B, keyFrom),
+        exit = A.filter(a => groupB[a[keyTo]] == null),
+        enter = B.filter(b => groupA[b[keyFrom]] == null)
 
-  const exit = reject(A, a => groupB[a[keyTo]] != null)
-  let [updated, enter] = partition(B, b => groupA[b[keyFrom]] != null)
+  let updating = [], updatedFrom = [],
+      updated = B.filter(b => groupA[b[keyFrom]] != null)
 
-  let updating = []
-  let updatedFrom = []
   for (let bIndex = 0; bIndex < updated.length; bIndex++) {
     const b = updated[bIndex],
           group = groupA[b[keyFrom]]
@@ -34,5 +42,5 @@ export default (A=[], B=[], options={}) => {
   const before = exit.concat(updating).concat(enter.map(options.enterFrom))
   const after = (exit.map(options.exitTo)).concat(updated).concat(enter)
 
-  return { exit, updating, updated, enter, before, after }
+  return { before, after, exit, updating, updated, enter }
 }
